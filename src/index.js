2a1f2c6bb2f272e1b88ff074e8ad4e427890960d
amaillo <amaillom@yahoo.com>
2023-04-29 20:46:33 -0400

    Changing version in "about".

--- src/index.js
+++ src/index.js
@@ -2698,7 +2698,7 @@ aboutDialogLayout.addWidget(aboutMadeByWidget)
 
 const aboutTitleLabel = new QLabel()
 aboutTitleLabel.setTextInteractionFlags(1)
-aboutTitleLabel.setText("MH Pointers Tool" +" - Ver. 1.0.0")
+aboutTitleLabel.setText("MH Pointers Tool" +" - Ver. 1.0.1")
 const aboutTitleLayout = new QBoxLayout(0)
 aboutTitleLayout.addWidget(aboutTitleLabel)
 aboutTitleWidget.setLayout(aboutTitleLayout)
